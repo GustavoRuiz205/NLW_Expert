83184190ed325cdbb730bf560d9e0420285da608
Gustavo <gustavoruizprestes@yahoo.com.br>
2024-02-08 12:17:11 -0300

    initial commit

--- script.js
+++ script.js
@@ -1,96 +1,54 @@
 const perguntas = [
   {
-    pergunta: "Qual é a finalidade do comando 'console.log()' em JavaScript?",
-    respostas: [
-      'Exibir uma mensagem de erro',
-      'Imprimir dados no console',
-      'Criar uma variável'
-    ],
+    pergunta: 'Qual é o nome completo do protagonista de Naruto?',
+    respostas: ['Naruto Uzumaki', 'Naruto Namikaze', 'Naruto Senju'],
+    correta: 0
+  },
+  {
+    pergunta: 'Quem é o sensei de Naruto na Academia Ninja?',
+    respostas: ['Kakashi Hatake', 'Jiraiya', 'Iruka Umino'],
+    correta: 2
+  },
+  {
+    pergunta: 'Qual é o nome do clã de Sasuke Uchiha?',
+    respostas: ['Clã Senju', 'Clã Hyuga', 'Clã Uchiha'],
+    correta: 2
+  },
+  {
+    pergunta: 'Qual é o nome do jutsu de assinatura de Sasuke?',
+    respostas: ['Rasengan', 'Chidori', 'Kage Bunshin no Jutsu'],
     correta: 1
   },
   {
-    pergunta: "Qual é a função do operador '===' em comparações em JavaScript?",
-    respostas: [
-      'Comparação de valores sem considerar o tipo',
-      'Atribuição de valores',
-      'Comparação estrita de valores e tipos'
-    ],
+    pergunta: 'Quem é o líder da Akatsuki?',
+    respostas: ['Zabuza Momochi', 'Itachi Uchiha', 'Pain (Nagato)'],
     correta: 2
   },
   {
-    pergunta: 'Como se declara uma variável em JavaScript?',
-    respostas: [
-      'let myVar;',
-      'const myVar = 10;',
-      'ambas as opções acima estão corretas'
-    ],
-    correta: 2
+    pergunta: 'Qual é o bijuu (demônio com cauda) dentro de Naruto?',
+    respostas: ['Kurama', 'Matatabi', 'Shukaku'],
+    correta: 0
   },
   {
-    pergunta: 'O que é uma função em JavaScript?',
-    respostas: [
-      'Um tipo de dado',
-      'Um bloco de código reutilizável',
-      'Uma variável global'
-    ],
+    pergunta: 'Qual é o nome da aldeia ninja onde Naruto e seus amigos vivem?',
+    respostas: ['Aldeia da Folha', 'Aldeia da Areia', 'Aldeia da Nuvem'],
+    correta: 0
+  },
+  {
+    pergunta: 'Quem é conhecido como o Copy Ninja em Naruto?',
+    respostas: ['Jiraiya', 'Kakashi Hatake', 'Orochimaru'],
     correta: 1
   },
   {
-    pergunta:
-      "Qual é a diferença entre 'let' e 'const' na declaração de variáveis?",
-    respostas: [
-      'Nenhuma, são sinônimos',
-      'let é usado para valores constantes, const para variáveis',
-      'let permite reatribuição, const cria variáveis imutáveis'
-    ],
-    correta: 2
-  },
-  {
-    pergunta: 'O que é o DOM em JavaScript?',
-    respostas: [
-      'Um método de criptografia',
-      'Um modelo de objeto para manipular documentos HTML',
-      'Uma linguagem de programação'
-    ],
+    pergunta: 'Qual é a habilidade especial de Rock Lee?',
+    respostas: ['Genjutsu', 'Taijutsu', 'Ninjutsu'],
     correta: 1
   },
   {
     pergunta:
-      'Como se realiza uma iteração sobre os elementos de um array em JavaScript?',
-    respostas: [
-      "Usando a estrutura 'if-else'",
-      "Com a declaração 'switch'",
-      "Utilizando loops como 'for' ou 'forEach'"
-    ],
-    correta: 2
-  },
-  {
-    pergunta: 'O que é o JSON em JavaScript?',
-    respostas: [
-      'Um método de formatação de texto',
-      'Uma linguagem de estilização',
-      'Um formato de dados leve e intercambiável'
-    ],
-    correta: 2
-  },
-  {
-    pergunta: "Qual é a diferença entre 'null' e 'undefined' em JavaScript?",
-    respostas: [
-      'São iguais, usados de forma intercambiável',
-      "'null' representa a ausência de valor, 'undefined' é atribuído explicitamente",
-      'Ambos representam valores vazios'
-    ],
-    correta: 1
-  },
-  {
-    pergunta:
-      'Como se adiciona um evento a um elemento HTML usando JavaScript?',
-    respostas: [
-      'Apenas com CSS',
-      "Usando o atributo 'event'",
-      "Através do método 'addEventListener'"
-    ],
-    correta: 2
+      'Qual é o nome do mentor de Naruto no modo de controle da Kyuubi?',
+    respostas: ['Killer Bee', 'Hiruzen Sarutobi', 'Jiraiya'],
+    correta: 0
   }
 ]
 
